ffa97bf1f0b21b1780a4d34ba77f8d959dece1ed
Brendon <brendon.lopes.21@gmail.com>
2022-08-10 16:19:52 -0300

    feat: :sparkles: Creates PVE class extending from Battle class

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,23 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+// import Monster from '../Monster';
+import Battle from './Battle';
+
+class PVE extends Battle {
+  constructor(player: Fighter, private _monsters: SimpleFighter[]) {
+    super(player);
+  }
+
+  fight(): number {
+    while (
+      this.player.lifePoints > 0
+      && this._monsters.every(({ lifePoints }) => lifePoints > 0)
+    ) {
+      this._monsters.forEach((monster) => this.player.attack(monster));
+      this._monsters.forEach((monster) => monster.attack(this.player));
+    }
+
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
+
+export default PVE;
